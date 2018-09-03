cc71b05dd66b5542538e0216450c9575e9df146f
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-03 16:59:00 -0600

    working on board test

--- lib/board.rb
+++ lib/board.rb
@@ -6,8 +6,17 @@ class Board
 
   def make_board
     cells = []
-    cells << ('A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4')
-    @board_grids = cells.map do |location|
-      cells.new(position)
+    cells.push('A1', 'A2', 'A3', 'A4',
+              'B1', 'B2', 'B3', 'B4',
+              'C1', 'C2', 'C3', 'C4',
+             'D1', 'D2', 'D3', 'D4')
+    @board_grids = cells.map do |point|
+      cells.new(point)
+    end
+    p @board_grids = @board_grid.each_slice(4).to_a
+  end
 
 end
+
+board = Board.new
+board.make_board
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -4,9 +4,12 @@ require "./lib/cell.rb"
 
 class CellMinitest < Minitest::Test
   def test_it_exist
-    cell = Cell.new("value")
+    cell = Cell.new("A3")
 
     assert_instance_of Cell, cell
   end
+
+  def test_it_has_attributes
+    cell = Cell.new("A3", )
 
 end
