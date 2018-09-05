5ea029bcf78b914e93d3828f942d4f561a952aea
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-04 19:45:46 -0600

    modified ship

---
+++ lib/ship.rb
@@ -0,0 +1,28 @@
+require "pry"
+require "./lib/cell"
+
+class Ship
+  attr_reader :length, :name, :size, :sink_counter
+
+  attr_accessor :sunk
+
+  def initialize(length, name = "", size = "")
+    @length = length
+    @name = name
+    @size = size
+    @sink_counter = 0
+  end
+
+  def take_hit
+    @length -= 1
+  end
+
+  def sunk
+    if @sink_counter == 0 && @length == 0
+      @sink_counter += 1
+      true
+    else
+      false
+    end
+  end
+end
